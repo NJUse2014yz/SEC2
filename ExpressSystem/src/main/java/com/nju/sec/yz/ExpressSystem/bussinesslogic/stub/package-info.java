1550225f8125aded2fcab54918de76c278b47e49
ZhouSaisai <1509403958@qq.com>
2015-10-24 16:41:49 +0800

    add and rename package

--- ExpressSystem/src/main/java/com/nju/sec/yz/ExpressSystem/bussinesslogic/stub/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author Administrator
- *
- */
-package com.nju.sec.yz.ExpressSystem.bussinesslogic.stub;
---
+++ ExpressSystem/src/main/java/nju/sec/yz/ExpressSystem/App.java
@@ -0,0 +1,13 @@
+package nju.sec.yz.ExpressSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
