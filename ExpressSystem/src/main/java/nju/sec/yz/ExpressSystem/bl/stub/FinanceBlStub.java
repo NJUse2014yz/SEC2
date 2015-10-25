bd9d6374a50864c91c78c3c072ae5cec6bef5ea1
ZhouSaisai <1509403958@qq.com>
2015-10-25 20:15:05 +0800

    financeblstub

---
+++ ExpressSystem/src/main/java/nju/sec/yz/ExpressSystem/bl/stub/FinanceBlStub.java
@@ -0,0 +1,59 @@
+package nju.sec.yz.ExpressSystem.bl.stub;
+
+import nju.sec.yz.ExpressSystem.blservice.accountBlService.FinanceBlSevice;
+import nju.sec.yz.ExpressSystem.common.ResultMessage;
+import nju.sec.yz.ExpressSystem.vo.AccountVO;
+import nju.sec.yz.ExpressSystem.vo.BussinessVO;
+import nju.sec.yz.ExpressSystem.vo.InitialVO;
+import nju.sec.yz.ExpressSystem.vo.OutVO;
+import nju.sec.yz.ExpressSystem.vo.PaymentSheetVO;
+import nju.sec.yz.ExpressSystem.vo.ProfitVO;
+import nju.sec.yz.ExpressSystem.vo.ReceiptVO;
+
+public class FinanceBlStub implements FinanceBlSevice {
+
+	@Override
+	public BussinessVO checkBusinessCircumstance(String begin, String end) {
+		// TODO 自动生成的方法存根
+		return new BussinessVO();
+	}
+
+	@Override
+	public ResultMessage checkReceipt(PaymentSheetVO rv, String day,
+			int positionId) {
+		// TODO 自动生成的方法存根
+		return ResultMessage.SUCCESS;
+	}
+
+	@Override
+	public ResultMessage makePayment(OutVO pro) {
+		// TODO 自动生成的方法存根
+		return ResultMessage.SUCCESS;
+	}
+
+	@Override
+	public ProfitVO makeCostReceipt() {
+		// TODO 自动生成的方法存根
+		return new ProfitVO(1000, 110, 890);
+	}
+
+	@Override
+	public void exportCostToExcel(ReceiptVO rv) {
+		// TODO 自动生成的方法存根
+		System.out.println("导出Excel文件成功");
+
+	}
+
+	@Override
+	public InitialVO observeIni() {
+		// TODO 自动生成的方法存根
+		return new InitialVO(null, null, null, null, null);
+	}
+
+	@Override
+	public ResultMessage initial(AccountVO av) {
+		// TODO 自动生成的方法存根
+		return ResultMessage.SUCCESS;
+	}
+
+}
