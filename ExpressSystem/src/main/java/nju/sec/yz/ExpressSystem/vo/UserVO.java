f033409fb735fc8f04120e52fc7ee10c87d100a2
congye6 <244053679@qq.com>
2015-10-25 10:26:49 +0800

    10.25
    UserVO

--- ExpressSystem/src/main/java/nju/sec/yz/ExpressSystem/vo/UserVO.java
+++ ExpressSystem/src/main/java/nju/sec/yz/ExpressSystem/vo/UserVO.java
@@ -1,5 +1,36 @@
 package nju.sec.yz.ExpressSystem.vo;
 
-public class UserVO {
 
+
+import nju.sec.yz.ExpressSystem.Power;
+
+public class UserVO {
+	private String id;
+	private String name;
+	private String password;
+	private Power  power;
+	public UserVO(String id,String name,String password,Power power){
+		this.id=id;
+		this.name=name;
+		this.password=password;
+		//power初始化
+		this.power=power;
+	}
+	public String getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public Power getPower() {
+		return power;
+	}
+	
 }
+
