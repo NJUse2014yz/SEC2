3c0e7bdc268d85a641f69a48f47ecb273bfae1d6
Augustoe <iangiuizq@163.com>
2015-10-25 19:42:30 +0800

    try

--- ExpressSystem/src/main/java/nju/sec/yz/ExpressSystem/data/stub/InDataStub.java
+++ ExpressSystem/src/main/java/nju/sec/yz/ExpressSystem/data/stub/InDataStub.java
@@ -18,37 +18,31 @@ public class InDataStub implements InDataService{
 
 	@Override
 	public ResultMessage insert(InPO inpo) throws RemoteException {
-		// TODO 自动生成的方法存根
 		return ResultMessage.SUCCESS;
 	}
 
 	@Override
 	public InPO find(String id) throws RemoteException {
-		// TODO 自动生成的方法存根
 		return new InPO("20151023",12,"025001D001");
 	}
 
 	@Override
 	public ResultMessage delete(String id) throws RemoteException {
-		// TODO 自动生成的方法存根
 		return ResultMessage.SUCCESS;
 	}
 
 	@Override
 	public ResultMessage update(InPO inpo) throws RemoteException {
-		// TODO 自动生成的方法存根
 		return ResultMessage.SUCCESS;
 	}
 
 	@Override
 	public ResultMessage init() throws RemoteException {
-		// TODO 自动生成的方法存根
 		return ResultMessage.SUCCESS;
 	}
 
 	@Override
 	public ArrayList<InPO> findAll() throws RemoteException {
-		// TODO 自动生成的方法存根
 		ArrayList<InPO> array=new ArrayList<InPO>();
 		array.add(new InPO("20151023",12,"025001D001"));
 		array.add(new InPO("20151024",30,"025002D004"));
