e7f1c8ec4e25179f6038febee08b752b0fd91299
ZhouSaisai <1509403958@qq.com>
2015-10-25 10:46:24 +0800

    try

--- ExpressSystem/src/main/java/nju/sec/yz/ExpressSystem/dataservice/userDataSevice/UserDataService.java
+++ ExpressSystem/src/main/java/nju/sec/yz/ExpressSystem/dataservice/userDataSevice/UserDataService.java
@@ -6,14 +6,14 @@ import java.rmi.RemoteException;
 import java.util.ArrayList;
 import nju.sec.yz.ExpressSystem.po.*;
 import nju.sec.yz.ExpressSystem.ResultMessage;
-import nju.sec.yz.ExpressSystem.po.UserPO;
+import nju.sec.yz.ExpressSystem.po.UserP;
 
 public interface UserDataService 
 {
-	public ResultMessage insert(UserPO upo) throws RemoteException;
-	public UserPO find(String id) throws RemoteException;
+	public ResultMessage insert(UserP upo) throws RemoteException;
+	public UserP find(String id) throws RemoteException;
 	public ResultMessage delete(String id) throws RemoteException;
-	public ResultMessage update(UserPO upo) throws RemoteException;
+	public ResultMessage update(UserP upo) throws RemoteException;
 	public ResultMessage init() throws RemoteException;
-	public ArrayList<UserPO> findAll() throws RemoteException;	
+	public ArrayList<UserP> findAll() throws RemoteException;	
 }
--- ExpressSystem/src/main/java/nju/sec/yz/ExpressSystem/po/UserP.java
+++ ExpressSystem/src/main/java/nju/sec/yz/ExpressSystem/po/UserP.java
@@ -1,12 +1,12 @@
 package nju.sec.yz.ExpressSystem.po;
 import nju.sec.yz.ExpressSystem.Power;
 
-public class UserPO {
+public class UserP {
 	private String id;
 	private String name;
 	private String password;
 	private Power  power;
-	public UserPO(String id,String name,String password,Power power){
+	public UserP(String id,String name,String password,Power power){
 		this.id=id;
 		this.name=name;
 		this.password=password;
