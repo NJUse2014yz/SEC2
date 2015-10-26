dc10c5a21992ba96cdb707954748f409ff52fb73
congye6 <244053679@qq.com>
2015-10-26 21:06:27 +0800

    Driver

--- ExpressSystem/src/main/java/nju/sec/yz/ExpressSystem/bl/stub/SalaryBlStub.java
+++ ExpressSystem/src/main/java/nju/sec/yz/ExpressSystem/bl/stub/SalaryBlStub.java
@@ -1,9 +1,12 @@
 package nju.sec.yz.ExpressSystem.bl.stub;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import nju.sec.yz.ExpressSystem.blservice.managerBlService.SalaryBlService;
+import nju.sec.yz.ExpressSystem.common.Power;
 import nju.sec.yz.ExpressSystem.common.ResultMessage;
+import nju.sec.yz.ExpressSystem.common.SalaryImformation;
 import nju.sec.yz.ExpressSystem.vo.SalaryVO;
 
 public class SalaryBlStub implements SalaryBlService {
@@ -17,7 +20,10 @@ public class SalaryBlStub implements SalaryBlService {
 	@Override
 	public ArrayList<SalaryVO> observeSalary() {
 		// TODO 自动生成的方法存根
-		return new ArrayList<SalaryVO>();
+		ArrayList<SalaryVO> salaryStrategy=new ArrayList<>();
+		SalaryVO salaryVO=new SalaryVO(new SalaryImformation(Power.MANAGER, 50));
+		salaryStrategy.add(salaryVO);
+		return salaryStrategy;
 	}
 
 }
