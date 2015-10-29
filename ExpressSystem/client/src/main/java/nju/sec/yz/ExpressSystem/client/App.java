39977034e19c5978dbd783ee951b1bef5a6ee644
ZhouSaisai <1509403958@qq.com>
2015-10-29 22:18:07 +0800

    changes coming

---
+++ ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/client/App.java
@@ -0,0 +1,13 @@
+package nju.sec.yz.ExpressSystem.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ExpressSystem/server/src/main/java/nju/sec/yz/ExpressSystem/server/App.java
@@ -0,0 +1,13 @@
+package nju.sec.yz.ExpressSystem.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
