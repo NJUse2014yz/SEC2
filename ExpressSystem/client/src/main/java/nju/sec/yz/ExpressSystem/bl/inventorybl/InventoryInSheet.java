3e0fc6c643a572e23a7f0021bba370f7d3bcfd5b
ZhouSaisai <1509403958@qq.com>
2015-11-07 10:40:51 +0800

    implement ReceiptService

--- ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/bl/inventorybl/InventoryInSheet.java
+++ ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/bl/inventorybl/InventoryInSheet.java
@@ -1,15 +1,32 @@
 package nju.sec.yz.ExpressSystem.bl.inventorybl;
 
+import nju.sec.yz.ExpressSystem.bl.receiptbl.ReceiptService;
 import nju.sec.yz.ExpressSystem.common.ResultMessage;
 import nju.sec.yz.ExpressSystem.vo.InventoryInSheetVO;
+import nju.sec.yz.ExpressSystem.vo.ReceiptVO;
 
 /**
  * 入库单的领域模型对象
  * @author 周聪
  *
  */
-public class InventoryInSheet {
-	public ResultMessage in(InventoryInSheetVO iisvo) {
+public class InventoryInSheet implements ReceiptService {
+	
+	@Override
+	public ResultMessage approve(ReceiptVO vo) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public ResultMessage make(ReceiptVO vo) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public ResultMessage modify(ReceiptVO vo) {
+		// TODO 自动生成的方法存根
 		return null;
 	}
 }
--- ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/bl/inventorybl/InventoryOutSheet.java
+++ ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/bl/inventorybl/InventoryOutSheet.java
@@ -1,16 +1,33 @@
 package nju.sec.yz.ExpressSystem.bl.inventorybl;
 
+import nju.sec.yz.ExpressSystem.bl.receiptbl.ReceiptService;
 import nju.sec.yz.ExpressSystem.common.ResultMessage;
 import nju.sec.yz.ExpressSystem.vo.InventoryOutSheetVO;
+import nju.sec.yz.ExpressSystem.vo.ReceiptVO;
 
 /**
  * 出库单的领域模型对象
  * @author 周聪
  *
  */
-public class InventoryOutSheet {
-	public ResultMessage out(InventoryOutSheetVO iosvo) {
-		// TODO Auto-generated method stub
+public class InventoryOutSheet implements ReceiptService{
+
+	@Override
+	public ResultMessage approve(ReceiptVO vo) {
+		// TODO 自动生成的方法存根
 		return null;
 	}
+
+	@Override
+	public ResultMessage make(ReceiptVO vo) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public ResultMessage modify(ReceiptVO vo) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+	
 }
