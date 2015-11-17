da7d1566df7217b31c706975208f9f28233752aa
congye6 <244053679@qq.com>
2015-11-17 19:37:06 +0800

    ResultMessage

--- ExpressSystem/common/src/main/java/nju/sec/yz/ExpressSystem/common/ResultMessage.java
+++ ExpressSystem/common/src/main/java/nju/sec/yz/ExpressSystem/common/ResultMessage.java
@@ -1,5 +1,31 @@
 package nju.sec.yz.ExpressSystem.common;
-
+/**
+ * 返回信息
+ * @author 周聪
+ */
 public class ResultMessage {
 
+	private Result result;
+	
+	private String message;//备注
+	
+	public ResultMessage(){
+		result=Result.SUCCESS;
+		message="success";
+	}
+	
+	public ResultMessage(String message){
+		result=Result.FAIL;
+		this.message=message;
+	}
+
+	public Result getResult() {
+		return result;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	
+	
 }
