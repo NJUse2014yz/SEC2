074bad5e057806c0d4eabdac080566329f5599a2
Augustoe <iangiuizq@163.com>
2015-11-19 09:56:53 +0800

    deliverReceive

--- ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/presentation/deliverui/DeliverOrderSearchUi.java
+++ ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/presentation/deliverui/DeliverOrderSearchUi.java
@@ -6,12 +6,16 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.JTableHeader;
 
 import nju.sec.yz.ExpressSystem.bl.deliverbl.Deliver;
 import nju.sec.yz.ExpressSystem.bl.deliverbl.DeliverController;
@@ -59,7 +63,7 @@ public class DeliverOrderSearchUi extends JPanel {
 					OrderVO ordervo=deliver.checkDeliver(searchnumber.getText());
 					OrderInformation orderInformation=ordervo.getOrderInformation();
 					
-					//快递单号填写错误的情况如何考虑
+//					快递单号填写错误的情况如何考虑
 					if(orderInformation==null){
 						warning=new JLabel();
 						warning.setText("输入信息错误");
@@ -69,6 +73,20 @@ public class DeliverOrderSearchUi extends JPanel {
 						add(warning);
 						repaint();
 					}else{
+						
+						JScrollPane scp=new JScrollPane();
+						scp.setBounds(144,105,315,177);
+						add(scp);
+						ArrayList<String> temp=orderInformation.getTrail();
+						int count=21+temp.size();
+						Object[][] info=new Object[count][2];
+						count=0;
+						
+						
+						String[] title={"寄件人姓名","住址","单位","电话","手机"};
+						JTable tab=new JTable(info,null);
+						JTableHeader jth=tab.getTableHeader();
+						scp.getViewport().add(tab);
 						//依次输出快递信息
 						
 					}
@@ -88,6 +106,29 @@ public class DeliverOrderSearchUi extends JPanel {
 			searchnumber = new JTextField();
 			searchnumber.setBounds(221, 72, 219, 20);
 			add(searchnumber);
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+//			
+//			
+//			JScrollPane scp=new JScrollPane();
+//			scp.setBounds(144,105,315,177);
+//			add(scp);
+//			Object[][] info=new Object[12][2];
+//			
+//			
+//			String[] title={"寄件人姓名","住址","单位","电话","手机"};
+//			JTable tab=new JTable(info,null);
+//			JTableHeader jth=tab.getTableHeader();
+//			scp.getViewport().add(tab);
+			
 			
 			
 		}
