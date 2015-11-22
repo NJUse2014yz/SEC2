5d7468ef317fdb34d41751baaf9f2f5ffa6a09de
ZhouSaisai <1509403958@qq.com>
2015-11-22 23:23:03 +0800

    staff

--- ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/bl/managerbl/Staff.java
+++ ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/bl/managerbl/Staff.java
@@ -1,8 +1,16 @@
 package nju.sec.yz.ExpressSystem.bl.managerbl;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 
+import nju.sec.yz.ExpressSystem.client.DatafactoryProxy;
+import nju.sec.yz.ExpressSystem.common.Result;
 import nju.sec.yz.ExpressSystem.common.ResultMessage;
+import nju.sec.yz.ExpressSystem.common.Status;
+import nju.sec.yz.ExpressSystem.dataservice.manageDataSevice.StaffDataService;
+import nju.sec.yz.ExpressSystem.po.CarPO;
+import nju.sec.yz.ExpressSystem.po.StaffPO;
+import nju.sec.yz.ExpressSystem.vo.CarVO;
 import nju.sec.yz.ExpressSystem.vo.StaffVO;
 
 /**
@@ -11,33 +19,114 @@ import nju.sec.yz.ExpressSystem.vo.StaffVO;
  *
  */
 public class Staff {
+	private StaffDataService data;
 	
+	public Staff(){
+		try {
+			data=DatafactoryProxy.getStaffDataService();
+		} catch (RemoteException e) {
+			//TODO 远程异常
+			e.printStackTrace();
+		}
+	}
 	public ResultMessage addStaff(StaffVO sv) {
-		// TODO Auto-generated method stub
-		return null;
+		ResultMessage message=null;
+		//验证information
+		String validresult=isValid(sv);
+		if(!validresult.equals("success")){
+			System.out.println(validresult);
+			return new ResultMessage(Result.FAIL,validresult);
+		}
+		//创建PO并保存
+		try {
+			StaffPO po=changeVoToPo(sv);
+			message=data.insert(po);
+		} catch (RemoteException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+			return new ResultMessage(Result.FAIL,"系统错误");
+		}
+		return message;
 	}
 
 	
 	public ResultMessage deleteStaff(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		ResultMessage result=null;
+		//调用data层方法,验证id是否存在
+		try {
+			result=data.delete(id);
+		} catch (RemoteException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+			return new ResultMessage(Result.FAIL,"系统错误");
+		}
+		return result;
 	}
 
 
 	public ResultMessage modifyStaff(StaffVO sv) {
-		// TODO Auto-generated method stub
-		return null;
+		ResultMessage message=null;
+		//验证改过之后的vo
+		String validresult=isValid(sv);
+		if(!validresult.equals("success"))
+			return new ResultMessage(Result.FAIL,validresult);
+		//vo转po,数据库更新po
+		StaffPO po=changeVoToPo(sv);
+		try {
+			message=data.update(po);
+		} catch (RemoteException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+			return new ResultMessage(Result.FAIL,"系统错误");
+		}
+		return message;
 	}
 
 	
 	public StaffVO observeStaff(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		StaffVO vo=null;
+		try {
+			StaffPO	po = data.find(id);
+			vo=changePoToVo(po);
+		} catch (RemoteException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+		return vo;
 	}
 
 	
 	public ArrayList<StaffVO> observeStaff() {
-		// TODO Auto-generated method stub
+		ArrayList<StaffPO> listPO = null;
+		ArrayList<StaffVO> listVO = new ArrayList<StaffVO>();
+		//获取数据库中的userpo列表
+		try {
+			listPO=data.findAll();
+		} catch (RemoteException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+		//将userpo列表转换成uservo列表
+		for(int i=0;i<listPO.size();i++){
+			StaffVO vo=changePoToVo(listPO.get(i));
+			listVO.add(vo);
+		}
+		return listVO;
+	}
+	private StaffVO changePoToVo(StaffPO po) {
+		String name=po.getName();
+		String id=po.getId();
+		Status power=po.getPower();
+		String agency=po.getAgency().getName();
+		StaffVO vo=new StaffVO(name, id, power, agency);
+		return vo;
+	}
+	private StaffPO changeVoToPo(StaffVO sv) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+	private String isValid(StaffVO sv) {
+		// TODO 自动生成的方法存根
 		return null;
 	}
 }
