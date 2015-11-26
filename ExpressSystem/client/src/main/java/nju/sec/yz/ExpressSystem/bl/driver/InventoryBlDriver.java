df5b6efa1e3ff1c5512292211614e0c25e7bac7b
ZhouSaisai <1509403958@qq.com>
2015-11-26 12:59:31 +0800

    bl and driver

--- ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/bl/driver/InventoryBlDriver.java
+++ ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/bl/driver/InventoryBlDriver.java
@@ -1,5 +1,7 @@
 package nju.sec.yz.ExpressSystem.bl.driver;
 
+import java.util.ArrayList;
+
 import nju.sec.yz.ExpressSystem.bl.stub.InventoryBlStub;
 import nju.sec.yz.ExpressSystem.blservice.inventoryBlService.InventoryBlService;
 import nju.sec.yz.ExpressSystem.common.ResultMessage;
@@ -16,8 +18,8 @@ public class InventoryBlDriver {
 		inventoryBlService.exportToExcel();
 		System.out.println("单据导出成功");
 		// 2库存盘点
-		InventoryVO ivo1 = inventoryBlService.checkStock();
-		System.out.println(ivo1.getInventoryInInformation() + "[没有信息吗]" + ivo1.getInventoryOutInformation());
+		ArrayList<InventoryVO> ivo1 = inventoryBlService.checkStock();
+		System.out.println(ivo1.get(0).getInventoryInInformation() + "[没有信息吗]" + ivo1.get(0).getInventoryOutInformation());
 		// 3库存查看
 		
 		
--- ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/bl/stub/InventoryBlStub.java
+++ ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/bl/stub/InventoryBlStub.java
@@ -1,5 +1,7 @@
 package nju.sec.yz.ExpressSystem.bl.stub;
 
+import java.util.ArrayList;
+
 import nju.sec.yz.ExpressSystem.blservice.inventoryBlService.InventoryBlService;
 import nju.sec.yz.ExpressSystem.common.ResultMessage;
 import nju.sec.yz.ExpressSystem.vo.InventoryInSheetVO;
@@ -9,14 +11,15 @@ import nju.sec.yz.ExpressSystem.vo.InventoryVO;
 public class InventoryBlStub implements InventoryBlService {
 
 	@Override
-	public void exportToExcel() {
+	public ResultMessage exportToExcel() {
 		System.out.println("导出Excel文件成功");
+		return null;
 	}
 
 	@Override
-	public InventoryVO checkStock() {
+	public ArrayList<InventoryVO> checkStock() {
 		// TODO 自动生成的方法存根
-		return new InventoryVO();
+		return null;
 	}
 
 	@Override
@@ -40,7 +43,7 @@ public class InventoryBlStub implements InventoryBlService {
 
 
 	@Override
-	public InventoryVO observeStock(String transit, String begin, String end) {
+	public ArrayList<InventoryVO> observeStock(String transit, String begin, String end) {
 		// TODO Auto-generated method stub
 		return null;
 	}
