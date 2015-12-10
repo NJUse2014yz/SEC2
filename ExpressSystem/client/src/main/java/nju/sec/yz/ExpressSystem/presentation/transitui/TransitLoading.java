bbb85b685df0444aaa4a1bee8a6f20db5069d61a
ZhouSaisai <1509403958@qq.com>
2015-12-10 10:38:54 +0800

    ？

--- ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/presentation/transitui/TransitLoading.java
+++ ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/presentation/transitui/TransitLoading.java
@@ -167,7 +167,7 @@ public class TransitLoading extends JPanel{
 						for(int i=0;i<barId.getRowCount()-1;i++){
 							BarIdArray.add((String) barId.getValueAt(i, 1));
 						}
-						System.out.println(BarIdArray.get(3));
+						//System.out.println(BarIdArray.get(3));
 					TransitLoadSheetVO vo=new TransitLoadSheetVO();
 						//destinationId项不存在，用null写入
 					LoadInformation loadInf = new LoadInformation(date.getTime(), agencyId.getSelectedItem().toString(),
