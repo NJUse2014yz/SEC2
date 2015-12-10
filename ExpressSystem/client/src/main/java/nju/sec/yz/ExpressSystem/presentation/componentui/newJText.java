61c7f2fdd37a9f909fcccdb8bdb454d29de0e3eb
Augustoe <iangiuizq@163.com>
2015-12-10 10:24:20 +0800

    change

---
+++ ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/presentation/componentui/newJText.java
@@ -0,0 +1,45 @@
+package nju.sec.yz.ExpressSystem.presentation.componentui;
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JTextField;
+
+public class newJText extends JTextField{
+	public newJText(){
+		iniText();
+	}
+	
+	public newJText(String str){
+		iniText();
+	}
+	
+	private void iniText(){
+		setBorder(BorderFactory.createLineBorder(Color.WHITE));
+		setForeground(Color.WHITE);
+		setOpaque(false);
+		
+		addMouseListener(new MouseAdapter(){
+
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				setBorder(BorderFactory.createLineBorder(Color.WHITE));
+			}
+		});
+	}
+	
+	
+	
+	
+}
