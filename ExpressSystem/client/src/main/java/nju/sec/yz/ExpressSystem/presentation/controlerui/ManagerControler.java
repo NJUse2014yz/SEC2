703a9836b44f8d1e180cb6fd9c30bb0749320c05
congye6 <244053679@qq.com>
2015-12-26 10:17:51 +0800

    1

--- ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/presentation/controlerui/ManagerControler.java
+++ ExpressSystem/client/src/main/java/nju/sec/yz/ExpressSystem/presentation/controlerui/ManagerControler.java
@@ -22,98 +22,98 @@ import nju.sec.yz.ExpressSystem.presentation.managerui.ManagerStaffModify;
 import nju.sec.yz.ExpressSystem.presentation.managerui.ManagerStaffObserve;
 
 public class ManagerControler {
-private ClientControler mainControler;
-public ManagerButtonComponent mbc;
+	private ClientControler mainControler;
+	public ManagerButtonComponent mbc;
 
-	
-	public ManagerControler(ClientControler mainControler)
-	{
-		this.mainControler=mainControler;
-		this.mbc=new ManagerButtonComponent(mainControler);
+	public ManagerControler(ClientControler mainControler) {
+		this.mainControler = mainControler;
+		this.mbc = new ManagerButtonComponent(mainControler);
 	}
-	public void managerChangePanel(MANAGER_CONTROL n)
-	{
-		switch(n)
-		{
+
+	public void managerChangePanel(MANAGER_CONTROL n) {
+		switch (n) {
 		case MANAGER_MAIN:
-			mainControler.mainFrame.nextPanel(new ManagerMainUi(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerMainUi(mainControler, mbc));
 			break;
 		case AGENCY_ADD:
-			mainControler.mainFrame.nextPanel(new ManagerAgencyAdd(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerAgencyAdd(mainControler, mbc));
 			break;
 		case AGENCY_ADD_SUCCESS:
 			break;
 		case AGENCY_DELETE:
-			mainControler.mainFrame.nextPanel(new ManagerAgencyDelete(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerAgencyDelete(mainControler, mbc));
 			break;
 		case AGENCY_MODIFY:
-//			mainControler.mainFrame.nextPanel(new ManagerAgencyModify(mainControler,mbc));
+			// mainControler.mainFrame.nextPanel(new
+			// ManagerAgencyModify(mainControler,mbc));
 			break;
 		case AGENCY_LIST:
-			mainControler.mainFrame.nextPanel(new ManagerAgencyList(mainControler,mbc,"modify"));
+			mainControler.mainFrame.nextPanel(new ManagerAgencyList(mainControler, mbc, "modify"));
 			break;
 		case AGENCY_MODIFY_SUCCESS:
 			break;
 		case AGENCY_INQUIRY:
-			mainControler.mainFrame.nextPanel(new ManagerAgencyList(mainControler,mbc,"observe"));
+			mainControler.mainFrame.nextPanel(new ManagerAgencyList(mainControler, mbc, "observe"));
 			break;
 		case STAFF_ADD:
-			mainControler.mainFrame.nextPanel(new ManagerStaffAdd(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerStaffAdd(mainControler, mbc));
 			break;
 		case STAFF_ADD_SUCCESS:
 			break;
 		case STAFF_DELETE:
-			mainControler.mainFrame.nextPanel(new ManagerStaffDelete(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerStaffDelete(mainControler, mbc));
 			break;
 		case STAFF_MODIFY:
-//			mainControler.mainFrame.nextPanel(new ManagerStaffModify(mainControler,mbc));
+			// mainControler.mainFrame.nextPanel(new
+			// ManagerStaffModify(mainControler,mbc));
 			break;
 		case STAFF_LIST:
-			mainControler.mainFrame.nextPanel(new ManagerStaffList(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerStaffList(mainControler, mbc));
 			break;
 		case STAFF_MODIFY_SUCCESS:
 			break;
 		case STAFF_INQUIRY:
-			mainControler.mainFrame.nextPanel(new ManagerStaffObserve(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerStaffObserve(mainControler, mbc));
 			break;
 		case SALARY_MODIFY:
-			mainControler.mainFrame.nextPanel(new ManagerSalaryModify(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerSalaryModify(mainControler, mbc));
 			break;
 		case SALARY_INQUIRY:
-			mainControler.mainFrame.nextPanel(new ManagerSalaryObserve(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerSalaryObserve(mainControler, mbc));
 			break;
 		case CONST_ADD:
-			mainControler.mainFrame.nextPanel(new ManagerConstAdd(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerConstAdd(mainControler, mbc));
 			break;
 		case CONST_ADD_SUCCESS:
 			break;
 		case CONST_MODIFY:
-			mainControler.mainFrame.nextPanel(new ManagerConstModify(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerConstModify(mainControler, mbc));
 			break;
 		case CONST_MODIFY_SUCCESS:
 			break;
 		case CONST_INQUIRY:
-			mainControler.mainFrame.nextPanel(new ManagerConstObserve(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerConstObserve(mainControler, mbc));
 			break;
 		case RECEIPT_LIST:
-			mainControler.mainFrame.nextPanel(new ManagerReceiptList(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerReceiptList(mainControler, mbc));
 			break;
 		case RECEIPT_MODIFY:
-//			mainControler.mainFrame.nextPanel(new ManagerReceiptModify(mainControler,mbc));
+			// mainControler.mainFrame.nextPanel(new
+			// ManagerReceiptModify(mainControler,mbc));
 			break;
 		case ACCOUNT_CHECK:
-			mainControler.mainFrame.nextPanel(new ManagerAccountCheck(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerAccountCheck(mainControler, mbc));
 			break;
 		case COST_CHECK:
 			break;
 		case OPERATE_CHECK:
 			break;
 		case LOG_CHECK:
-			mainControler.mainFrame.nextPanel(new ManagerLogCheck(mainControler,mbc));
+			mainControler.mainFrame.nextPanel(new ManagerLogCheck(mainControler, mbc));
 			break;
 		default:
 			System.out.println("wrong control");
-			break;	
+			break;
 		}
 	}
 
